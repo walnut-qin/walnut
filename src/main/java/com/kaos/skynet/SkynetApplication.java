d62287c7c35aa387e2c02135deb8b384a07aa1f8
nyakori <935419854@qq.com>
2022-07-12 10:56:42 +0800

    init(all): 初始化sprignboot框架

---
+++ src/main/java/com/kaos/skynet/SkynetApplication.java
@@ -0,0 +1,13 @@
+package com.kaos.skynet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkynetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkynetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaos/skynet/SkynetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaos.skynet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkynetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
