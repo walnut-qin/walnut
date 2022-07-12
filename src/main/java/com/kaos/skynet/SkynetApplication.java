44b1668acfe9a94e16248526470e486479ec7321
nyakori <935419854@qq.com>
2022-07-12 16:44:47 +0800

    feat(all): 添加第一个mapper用于测试

--- src/main/java/com/kaos/skynet/SkynetApplication.java
+++ src/main/java/com/kaos/skynet/SkynetApplication.java
@@ -1,8 +1,10 @@
 package com.kaos.skynet;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.kaos.skynet.core.api.data.mapper")
 @SpringBootApplication
 public class SkynetApplication {
 
---
+++ src/main/java/com/kaos/skynet/core/type/enums/SexEnum.java
@@ -0,0 +1,24 @@
+package com.kaos.skynet.core.type.enums;
+
+import com.kaos.skynet.core.type.Enum;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum SexEnum implements Enum {
+    Male("M", "男"),
+    Female("F", "女"),
+    Unknown("U", "不详");
+
+    /**
+     * 数据库存值
+     */
+    private String value;
+
+    /**
+     * 描述存值
+     */
+    private String description;
+}
