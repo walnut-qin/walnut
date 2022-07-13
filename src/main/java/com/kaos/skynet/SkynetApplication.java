18cf0b546ee8e0afa9160cd18122aefc430ecad7
nyakori <935419854@qq.com>
2022-07-13 18:56:07 +0800

    feat(api): 新增docare麻醉字典接口

--- src/main/java/com/kaos/skynet/SkynetApplication.java
+++ src/main/java/com/kaos/skynet/SkynetApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@MapperScan("com.kaos.skynet.core.api.data.mapper")
+@MapperScan({ "com.kaos.skynet.core.api.data.mapper", "com.kaos.skynet.api.data.docare.mapper" })
 @SpringBootApplication
 public class SkynetApplication {
 
