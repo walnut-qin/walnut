abda11f4a58b171101bc5119cfe7ae11c141f107
nyakori <935419854@qq.com>
2022-07-15 10:59:57 +0800

    add(mapper): 新增挂号表接口

---
+++ src/main/java/com/kaos/skynet/api/data/his/mapper/xyhis/FinOprRegisterMapper.java
@@ -0,0 +1,8 @@
+package com.kaos.skynet.api.data.his.mapper.xyhis;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kaos.skynet.api.data.his.entity.xyhis.FinOprRegister;
+
+public interface FinOprRegisterMapper extends BaseMapper<FinOprRegister> {
+    
+}
