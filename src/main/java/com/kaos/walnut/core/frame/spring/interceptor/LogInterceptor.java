fec1340318afe7b8d07028ed13a19a54315c325e
nyakori <935419854@qq.com>
2022-07-17 19:50:24 +0800

    fix(core): 优化日志模块，将LogAdvice嵌入到LogInterceptor

--- src/main/java/com/kaos/walnut/core/frame/spring/interceptor/LogInterceptor.java
+++ src/main/java/com/kaos/walnut/core/frame/spring/interceptor/LogInterceptor.java
@@ -1,7 +1,9 @@
 package com.kaos.walnut.core.frame.spring.interceptor;
 
+import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,18 +13,23 @@ import com.kaos.walnut.core.type.annotations.ApiName;
 import com.kaos.walnut.core.util.ObjectUtils;
 import com.kaos.walnut.core.util.StringUtils;
 
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdvice;
 
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
-public class LogInterceptor implements HandlerInterceptor {
+class LogInterceptor implements HandlerInterceptor {
     /**
      * 日志构造器
      */
-    public static ThreadLocal<StringBuilder> logBuilder = new ThreadLocal<>();
+    static ThreadLocal<StringBuilder> logBuilder = new ThreadLocal<>();
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
@@ -75,5 +82,44 @@ public class LogInterceptor implements HandlerInterceptor {
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
             throws Exception {
         logBuilder.remove();
+    }
+
+    /**
+     * 内置一个Advice用于处理带Body的日志
+     */
+    @ControllerAdvice
+    static class LogAdvice implements RequestBodyAdvice {
+        @Override
+        public boolean supports(MethodParameter methodParameter, Type targetType,
+                Class<? extends HttpMessageConverter<?>> converterType) {
+            return true;
+        }
+
+        @Override
+        public HttpInputMessage beforeBodyRead(HttpInputMessage inputMessage, MethodParameter parameter,
+                Type targetType,
+                Class<? extends HttpMessageConverter<?>> converterType) throws IOException {
+            return inputMessage;
+        }
+
+        @Override
+        public Object afterBodyRead(Object body, HttpInputMessage inputMessage, MethodParameter parameter,
+                Type targetType,
+                Class<? extends HttpMessageConverter<?>> converterType) {
+            // 加入body
+            logBuilder.get().append(String.format(" body = %s", ObjectUtils.serialize(body)));
+
+            // 写日志
+            log.info(logBuilder.get().toString());
+
+            // 透传body
+            return body;
+        }
+
+        @Override
+        public Object handleEmptyBody(Object body, HttpInputMessage inputMessage, MethodParameter parameter,
+                Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+            return body;
+        }
     }
 }
