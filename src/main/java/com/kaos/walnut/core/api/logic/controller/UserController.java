35f52a80c7742ae9f152e9a30b66854acdfe0e57
nyakori <935419854@qq.com>
2022-07-18 09:18:32 +0800

    fix(core): 更新核心api，改名

--- src/main/java/com/kaos/walnut/core/api/logic/controller/UserController.java
+++ src/main/java/com/kaos/walnut/core/api/logic/controller/UserController.java
@@ -2,25 +2,23 @@ package com.kaos.walnut.core.api.logic.controller;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.net.Inet4Address;
-import java.util.List;
 import java.util.Map;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 
 import com.google.common.collect.Maps;
+import com.kaos.walnut.core.api.data.cache.KaosUserCache;
+import com.kaos.walnut.core.api.data.cache.KaosUserRoleCache;
 import com.kaos.walnut.core.api.data.entity.KaosUser;
-import com.kaos.walnut.core.api.data.mapper.KaosUserMapper;
-import com.kaos.walnut.core.api.data.mapper.KaosUserRoleMapper;
 import com.kaos.walnut.core.api.logic.service.TokenService;
+import com.kaos.walnut.core.tool.Environment;
 import com.kaos.walnut.core.type.MediaType;
 import com.kaos.walnut.core.type.annotations.ApiName;
 import com.kaos.walnut.core.type.annotations.PassToken;
 import com.kaos.walnut.core.util.ObjectUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -43,19 +41,19 @@ public class UserController {
      * 环境
      */
     @Autowired
-    Environment environment;
+    Environment env;
 
     /**
      * 用户接口
      */
     @Autowired
-    KaosUserMapper kaosUserMapper;
+    KaosUserCache kaosUserCache;
 
     /**
      * 用户角色
      */
     @Autowired
-    KaosUserRoleMapper kaosUserRoleMapper;
+    KaosUserRoleCache kaosUserRoleCache;
 
     /**
      * token操作业务
@@ -70,8 +68,8 @@ public class UserController {
      */
     @PassToken
     @ApiName("登陆系统")
-    @RequestMapping(value = "login", method = RequestMethod.POST, produces = MediaType.JSON)
-    Login.RspBody login(@RequestBody @Valid Login.ReqBody reqBody) {
+    @RequestMapping(value = "signIn", method = RequestMethod.POST, produces = MediaType.JSON)
+    Login.RspBody signIn(@RequestBody @Valid Login.ReqBody reqBody) {
         // 校验并生成token
         String token = tokenService.genToken(reqBody.username, reqBody.password);
 
@@ -111,47 +109,22 @@ public class UserController {
      * @return
      */
     @ApiName("获取用户信息")
-    @RequestMapping(value = "info", method = RequestMethod.GET, produces = MediaType.JSON)
-    Info.RspBody info() throws Exception {
+    @RequestMapping(value = "getInfo", method = RequestMethod.GET, produces = MediaType.JSON)
+    Map<String, Object> getInfo() throws Exception {
         // 获取登入账号信息
         var kaosUser = KaosUser.getCurrentUser();
 
         // 检索角色信息
-        Map<String, Object> key = Maps.newHashMap();
-        key.put("USER_CODE", kaosUser.getUserCode());
-        var kaosUserRoles = kaosUserRoleMapper.selectByMap(key);
+        var kaosUserRoles = kaosUserRoleCache.get(kaosUser.getUserCode());
 
         // 构造响应
-        var rspBuilder = Info.RspBody.builder();
-        rspBuilder.name(kaosUser.getUserName());
-        rspBuilder.avatar(String.format("http://%s:%s/api/user/avatar?userCode=%s",
-                Inet4Address.getLocalHost().getHostAddress(),
-                environment.getProperty("local.server.port"),
-                kaosUser.getUserCode()));
-        rspBuilder.roles(kaosUserRoles.stream().map(x -> {
+        Map<String, Object> result = Maps.newHashMap();
+        result.put("name", kaosUser.getUserName());
+        result.put("avatar", env.getLocalAddress().concat("/api/user/getAvatar?user=").concat(kaosUser.getUserCode()));
+        result.put("roles", kaosUserRoles.stream().map(x -> {
             return x.getRole();
         }).toList());
-        return rspBuilder.build();
-    }
-
-    static class Info {
-        @Builder
-        static class RspBody {
-            /**
-             * 用户密码
-             */
-            String name;
-
-            /**
-             * 头像
-             */
-            String avatar;
-
-            /**
-             * 角色
-             */
-            List<String> roles;
-        }
+        return result;
     }
 
     /**
@@ -160,8 +133,8 @@ public class UserController {
      * @return
      */
     @ApiName("注销")
-    @RequestMapping(value = "logout", method = RequestMethod.POST, produces = MediaType.JSON)
-    Object logout() {
+    @RequestMapping(value = "signOut", method = RequestMethod.POST, produces = MediaType.JSON)
+    Object signOut() {
         return ObjectUtils.EMPTY;
     }
 
@@ -172,10 +145,10 @@ public class UserController {
      */
     @PassToken
     @ApiName("获取头像")
-    @RequestMapping(value = "avatar", method = RequestMethod.GET, produces = MediaType.PNG)
-    BufferedImage avatar(@NotBlank(message = "用户编码不能为空") String userCode) throws IOException {
+    @RequestMapping(value = "getAvatar", method = RequestMethod.GET, produces = MediaType.PNG)
+    BufferedImage getAvatar(@NotBlank(message = "用户编码不能为空") String user) throws IOException {
         // 读取用户
-        var kaosUser = kaosUserMapper.selectById(userCode);
+        var kaosUser = kaosUserCache.get(user);
         if (kaosUser == null || kaosUser.getAvatar() == null || kaosUser.getAvatar().length == 0) {
             return null;
         }
