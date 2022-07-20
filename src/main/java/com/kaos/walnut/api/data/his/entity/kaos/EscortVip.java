cca63bd8100b2775f709365a76bb6191e679600d
nyakori <935419854@qq.com>
2022-07-20 09:19:17 +0800

    add(mapper): 添加陪护VIP表mapper

---
+++ src/main/java/com/kaos/walnut/api/data/his/entity/kaos/EscortVip.java
@@ -0,0 +1,46 @@
+package com.kaos.walnut.api.data.his.entity.kaos;
+
+import java.time.LocalDateTime;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import lombok.Data;
+
+@Data
+@TableName("KAOS.ESCORT_VIP")
+public class EscortVip {
+    /**
+     * 患者卡号
+     */
+    @TableId("PATIENT_CARD_NO")
+    @TableField("PATIENT_CARD_NO")
+    String patientCardNo;
+
+    /**
+     * 住院证序号
+     */
+    @TableId("HAPPEN_NO")
+    @TableField("HAPPEN_NO")
+    Integer happenNo;
+
+    /**
+     * 陪护人卡号
+     */
+    @TableField("HELPER_CARD_NO")
+    String helperCardNo;
+
+    /**
+     * 操作日期
+     */
+    @TableField("OPER_DATE")
+    LocalDateTime operDate;
+
+    /**
+     * 备注
+     */
+    @TableField("REMARK")
+    String remark;
+
+}
---
+++ src/main/java/com/kaos/walnut/api/data/his/mapper/kaos/EscortVipMapper.java
@@ -0,0 +1,10 @@
+package com.kaos.walnut.api.data.his.mapper.kaos;
+
+import com.baomidou.dynamic.datasource.annotation.DS;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kaos.walnut.api.data.his.entity.kaos.EscortVip;
+
+@DS("his")
+public interface EscortVipMapper extends BaseMapper<EscortVip> {
+    
+}
