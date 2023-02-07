1d58027bb3a7d0222571e4ef2db633df3a491057
nyakori <935419854@qq.com>
2023-02-07 19:41:59 +0800

    add(controller): 添加日结新医保接口

--- src/main/java/com/kaos/walnut/api/logic/controller/inpatient/balance/report/ReportController.java
+++ src/main/java/com/kaos/walnut/api/logic/controller/inpatient/balance/report/ReportController.java
@@ -1,20 +1,18 @@
 package com.kaos.walnut.api.logic.controller.inpatient.balance.report;
 
 import java.time.LocalDateTime;
-import java.util.Map;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.google.common.collect.Maps;
+import com.google.common.base.Optional;
 import com.kaos.walnut.api.data.his.entity.xyhis.FinIpbBalanceHead;
 import com.kaos.walnut.api.data.his.mapper.xyhis.FinIpbBalanceHeadMapper;
+import com.kaos.walnut.core.type.Enum;
 import com.kaos.walnut.core.type.MediaType;
 import com.kaos.walnut.core.type.annotations.ApiName;
-import com.kaos.walnut.core.util.DoubleUtils;
-import com.kaos.walnut.core.util.StringUtils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
@@ -24,6 +22,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 @Validated
 @CrossOrigin
@@ -37,52 +38,73 @@ class ReportController {
     FinIpbBalanceHeadMapper finIpbBalanceHeadMapper;
 
     @ResponseBody
-    @ApiName("抽取日结信息")
-    @RequestMapping(value = "fetchInfo", method = RequestMethod.POST, produces = MediaType.JSON)
-    Map<String, Object> fetchInfo(@RequestBody @Valid FetchInfo.ReqBody reqBody) {
+    @ApiName("查询新医保日结")
+    @RequestMapping(value = "queryNewYbDayBalanceCost", method = RequestMethod.POST, produces = MediaType.JSON)
+    Double queryNewYbDayBalanceCost(@RequestBody @Valid QueryNewYbDayBalanceCost.ReqBody reqBody) {
         // 获取符合条件的未日结的信息
         var queryWrapper = new LambdaQueryWrapper<FinIpbBalanceHead>();
-        queryWrapper.eq(FinIpbBalanceHead::getBalanceOperCode, reqBody.operCode);
+        queryWrapper.eq(FinIpbBalanceHead::getBalanceOperCode, reqBody.balancer);
         queryWrapper.between(FinIpbBalanceHead::getBalanceDate, reqBody.beginDate, reqBody.endDate);
-        queryWrapper.eq(FinIpbBalanceHead::getDaybalanceFlag, false);
-        var balanceHeads = finIpbBalanceHeadMapper.selectList(queryWrapper).stream();
+        queryWrapper.eq(FinIpbBalanceHead::getPactCode, "18");
+        var balanceHeads = finIpbBalanceHeadMapper.selectList(queryWrapper);
 
-        // 构造响应
-        Map<String, Object> result = Maps.newHashMap();
-
-        // 医保
-        Map<String, Object> medIns = Maps.newHashMap();
-        {
-            // 过滤出新医保数据
-            var data = balanceHeads.filter(x -> StringUtils.equals(x.getPactCode(), "18")).toList();
-            medIns.put("pubCost", data.stream().mapToDouble(x -> DoubleUtils.eraseNull(x.getPubCost())).sum());
-            medIns.put("payCost", data.stream().mapToDouble(x -> DoubleUtils.eraseNull(x.getPayCost())).sum());
-        }
-        result.put("medIns", medIns);
+        // 算和
+        Double cost = balanceHeads.stream().mapToDouble(x -> {
+            return Optional.fromNullable(switch (reqBody.type) {
+                case PUB -> x.getPubCost();
+                case PAY -> x.getPayCost();
+            }).or(0.0);
+        }).sum();
 
         // 返回响应数据
-        return result;
+        return cost;
     }
 
-    static class FetchInfo {
+    static class QueryNewYbDayBalanceCost {
         static class ReqBody {
             /**
-             * 结算员编码
+             * 结算员
              */
             @NotBlank(message = "结算员不能为空")
-            String operCode;
+            String balancer;
 
             /**
-             * 日结开始时间
+             * 开始时间
              */
-            @NotNull(message = "日结开始时间不能为空")
+            @NotNull(message = "开始时间不能为空")
             LocalDateTime beginDate;
 
             /**
-             * 日结结束时间
+             * 截至时间
              */
-            @NotNull(message = "日结结束时间不能为空")
+            @NotNull(message = "截止时间不能为空")
             LocalDateTime endDate;
+
+            /**
+             * 类型
+             */
+            @NotNull(message = "查询类型不能为空")
+            TypeEnum type;
+
+            /**
+             * 类型
+             */
+            @Getter
+            @AllArgsConstructor
+            enum TypeEnum implements Enum {
+                PUB("1", "1"),
+                PAY("2", "2");
+
+                /**
+                 * 数据库存值
+                 */
+                String value;
+
+                /**
+                 * 描述存值
+                 */
+                String description;
+            }
         }
     }
 }
