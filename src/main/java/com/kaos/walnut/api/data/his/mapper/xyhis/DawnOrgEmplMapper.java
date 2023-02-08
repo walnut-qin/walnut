0d31af156ad1c8b1ee7466bfe4d4f802a7311d45
nyakori <935419854@qq.com>
2023-02-08 09:33:11 +0800

    refactor(all): 删除各项api

--- src/main/java/com/kaos/walnut/api/data/his/mapper/xyhis/DawnOrgEmplMapper.java
+++
@@ -1,8 +0,0 @@
-package com.kaos.walnut.api.data.his.mapper.xyhis;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.kaos.walnut.api.data.his.entity.xyhis.DawnOrgEmpl;
-
-public interface DawnOrgEmplMapper extends BaseMapper<DawnOrgEmpl> {
-    
-}
