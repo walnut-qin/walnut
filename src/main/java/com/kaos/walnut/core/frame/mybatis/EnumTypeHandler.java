d0d2ce959f703b3f4d4bf4d128ba4cb9d1e3ad45
nyakori <935419854@qq.com>
2023-02-08 10:03:05 +0800

    reactor(frame): 注释mybatis插件对Enum处理的代码

--- src/main/java/com/kaos/walnut/core/frame/mybatis/EnumTypeHandler.java
+++ src/main/java/com/kaos/walnut/core/frame/mybatis/EnumTypeHandler.java
@@ -1,4 +1,16 @@
-package com.kaos.walnut.core.frame.mybatis;
+/*********************************************************
+ * File: EnumTypeHandler.java
+ * Created Date: 2022-07-25
+ * Author: walnut(覃鹏展)
+ * 
+ * Description:
+ *  实现mybatis插件对枚举类型Enum<A, B>在读写数据库时的处理
+ *  - 使用 Enum 的 A 域对接数据库
+ * 
+ * Copyright (C) 2023 襄阳市中心医院
+ *********************************************************/
+
+ package com.kaos.walnut.core.frame.mybatis;
 
 import java.sql.CallableStatement;
 import java.sql.PreparedStatement;
@@ -28,28 +40,37 @@ public class EnumTypeHandler<E extends Enum> extends BaseTypeHandler<E> {
         this.classOfE = classOfE;
     }
 
+    /**
+     * 写数据库的操作函数
+     */
     @Override
     public void setNonNullParameter(PreparedStatement ps, int i, E parameter, JdbcType jdbcType) throws SQLException {
         ps.setString(i, parameter.getValue());
     }
 
     /**
-     * 将字符串转为枚举类型
+     * 将源字符串对照枚举值域转换为枚举对象
      * 
-     * @param source
-     * @return
+     * @param source 源字符串
+     * @return 枚举对象
      */
-    private E getValue(String source) {
+    private E getValue(String source) throws RuntimeException {
+        // 轮训所有枚举对象，依次对照值域
         for (E e : classOfE.getEnumConstants()) {
             if (e.getValue().equals(source)) {
                 return e;
             }
         }
+
+        // 若对照失败，则写错误日志
         String err = String.format("反序列化枚举类型[%s]时, 出现了未对照的值[%s]", classOfE.getName(), source);
-        log.warn(err);
+        log.error(err);
         throw new RuntimeException(err);
     }
 
+    /**
+     * 读数据库操作
+     */
     @Override
     public E getNullableResult(ResultSet rs, String columnName) throws SQLException {
         if (rs.getObject(columnName) == null) {
@@ -58,6 +79,9 @@ public class EnumTypeHandler<E extends Enum> extends BaseTypeHandler<E> {
         return getValue(rs.getString(columnName));
     }
 
+    /**
+     * 读数据库操作
+     */
     @Override
     public E getNullableResult(ResultSet rs, int columnIndex) throws SQLException {
         if (rs.getObject(columnIndex) == null) {
@@ -66,6 +90,9 @@ public class EnumTypeHandler<E extends Enum> extends BaseTypeHandler<E> {
         return getValue(rs.getString(columnIndex));
     }
 
+    /**
+     * 读数据库操作
+     */
     @Override
     public E getNullableResult(CallableStatement cs, int columnIndex) throws SQLException {
         if (cs.getObject(columnIndex) == null) {
