95bd0927ee3c5f33a34cddcf2fb80204e03e7d81
nyakori <935419854@qq.com>
2023-02-16 11:25:44 +0800

    add(data): 添加手术授权表接口

---
+++ src/main/java/com/kaos/walnut/api/data/entity/MetComIcdOperation.java
@@ -0,0 +1,47 @@
+package com.kaos.walnut.api.data.entity;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import lombok.Data;
+
+@Data
+@TableName("XYHIS.MET_COM_ICDOPERATION")
+public class MetComIcdOperation {
+    /**
+     * ICD编码
+     */
+    @TableId("ICD_CODE")
+    String icdCode;
+
+    /**
+     * 手术名
+     */
+    @TableField("ICD_NAME")
+    String icdName;
+
+    /**
+     * 科室编码
+     */
+    @TableField("DEPT_CODE")
+    String deptCode;
+
+    /**
+     * 手术名
+     */
+    @TableField("DEPT_NAME")
+    String deptName;
+
+    /**
+     * 医生编码
+     */
+    @TableField("DOCT_CODE")
+    String docCode;
+
+    /**
+     * 医生名
+     */
+    @TableField("DOCT_NAME")
+    String docName;
+}
---
+++ src/main/java/com/kaos/walnut/api/data/mapper/MetComIcdOperationMapper.java
@@ -0,0 +1,8 @@
+package com.kaos.walnut.api.data.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.kaos.walnut.api.data.entity.MetComIcdOperation;
+
+public interface MetComIcdOperationMapper extends BaseMapper<MetComIcdOperation> {
+
+}
