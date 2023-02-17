29d7f29fffd28ad0f0fd2175b28307bf02f2d618
nyakori <935419854@qq.com>
2023-02-17 10:30:01 +0800

    fix(logic): 批量操作加锁

--- src/main/java/com/kaos/walnut/api/logic/controller/surgery/DictionaryController.java
+++ src/main/java/com/kaos/walnut/api/logic/controller/surgery/DictionaryController.java
@@ -1,6 +1,8 @@
 package com.kaos.walnut.api.logic.controller.surgery;
 
 import com.kaos.walnut.api.logic.service.surgery.DictionaryService;
+import com.kaos.walnut.core.tool.lock.Lock;
+import com.kaos.walnut.core.tool.lock.LockExecutor;
 import com.kaos.walnut.core.type.MediaType;
 import com.kaos.walnut.core.type.annotations.ApiName;
 
@@ -16,6 +18,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/surgery/dictionary")
 public class DictionaryController {
+    /**
+     * 字典维护锁，批量操作，为保证安全性，锁数量设为1
+     */
+    static Lock lock = new Lock("手术字典维护锁", 1);
+
+    /**
+     * 字典操作业务
+     */
     @Autowired
     DictionaryService dictionaryService;
 
@@ -28,7 +38,12 @@ public class DictionaryController {
      */
     @ApiName("导入手术授权数据")
     @RequestMapping(value = "importSurgery", method = RequestMethod.POST, produces = MediaType.JSON)
-    Integer importSurgery(@RequestBody Workbook workbook) {
-        return this.dictionaryService.importSurgery(workbook);
+    Integer importSurgery(@RequestBody Workbook workbook) throws Exception {
+        // 敏感操作，带锁执行
+        LockExecutor.clear();
+        LockExecutor.link(lock, 0);
+        return LockExecutor.execute(() -> {
+            return this.dictionaryService.importSurgery(workbook);
+        });
     }
 }
