928170157270b56741494fb3f4aca51c3b0ee857
nyakori <935419854@qq.com>
2023-03-10 12:25:17 +0800

    fix(surgery): 删除不必要的业务

--- src/main/java/com/kaos/walnut/api/logic/service/surgery/ApplicationService.java
+++ src/main/java/com/kaos/walnut/api/logic/service/surgery/ApplicationService.java
@@ -1,12 +1,5 @@
 package com.kaos.walnut.api.logic.service.surgery;
 
-import java.util.Map;
-
-import javax.validation.constraints.NotBlank;
-
-import com.kaos.walnut.api.data.mapper.MetOpsApplyMapper;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
@@ -16,19 +9,5 @@ import org.springframework.validation.annotation.Validated;
 @Service
 @Validated
 public class ApplicationService {
-    /**
-     * 手术申请单接口
-     */
-    @Autowired
-    MetOpsApplyMapper metOpsApplyMapper;
 
-    /**
-     * 通过手术申请单号获取手术申请单信息
-     * 
-     * @param applyNo 手术申请单号
-     * @return
-     */
-    public Map<String, Object> getInfo(@NotBlank(message = "手术申请单号不能为空") String applyNo) {
-        return null;
-    }
 }
