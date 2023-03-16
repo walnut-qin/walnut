2c78bcf157516f55003b5d38f42e411c060b61b0
nyakori <935419854@qq.com>
2023-03-16 19:51:47 +0800

    fix(token): token机制改为20分钟有效期和10分钟的更换期

--- src/main/java/com/kaos/walnut/api/logic/service/TokenService.java
+++ src/main/java/com/kaos/walnut/api/logic/service/TokenService.java
@@ -54,7 +54,7 @@ public class TokenService {
     public String genToken(String uid) {
         var builder = JWT.create();
         builder.withClaim("uid", uid);
-        builder.withExpiresAt(OffsetDateTime.now().plus(Duration.ofSeconds(10)).toInstant());
+        builder.withExpiresAt(OffsetDateTime.now().plus(Duration.ofMinutes(20)).toInstant());
         return builder.sign(Algorithm.HMAC256(secret));
     }
 
