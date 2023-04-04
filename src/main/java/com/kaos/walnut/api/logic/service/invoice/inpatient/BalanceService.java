034d9998330d9c40ff6e8a63461d8c772152ac97
nyakori <935419854@qq.com>
2023-04-04 11:42:59 +0800

    fix(service): 写了一些发票业务

--- src/main/java/com/kaos/walnut/api/logic/service/invoice/inpatient/BalanceService.java
+++ src/main/java/com/kaos/walnut/api/logic/service/invoice/inpatient/BalanceService.java
@@ -8,7 +8,9 @@ import org.springframework.stereotype.Service;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.google.common.collect.Maps;
+import com.kaos.walnut.api.data.cache.DawnOrgEmplCache;
 import com.kaos.walnut.api.data.entity.FinIpbBalanceHead;
+import com.kaos.walnut.api.data.entity.FinComElectronicInvoice.BusinessTypeEnum;
 import com.kaos.walnut.api.data.enums.TransTypeEnum;
 import com.kaos.walnut.api.data.mapper.FinIpbBalanceHeadMapper;
 import com.kaos.walnut.core.util.LocalDateTimeUtils;
@@ -18,6 +20,12 @@ import com.kaos.walnut.core.util.LocalDateTimeUtils;
  */
 @Service
 public class BalanceService {
+    /**
+     * 职工缓存
+     */
+    @Autowired
+    DawnOrgEmplCache dawnOrgEmplCache;
+
     /**
      * 结算头表接口
      */
@@ -32,16 +40,17 @@ public class BalanceService {
      * @param weChatOrderNo
      * @param openId
      * @param sourceType
+     * @param placeCode
      * @return
      */
-    public Map<String, String> genBalanceInvoice(String invoiceNo, String alipayCode, String weChatOrderNo, String openId, String sourceType) {
+    public Map<String, String> genBalanceInvoice(String invoiceNo, String alipayCode, String weChatOrderNo, String openId, String sourceType, String placeCode) {
         // 声明局部数据
         Map<String, String> result = Maps.newHashMap();
         Map<String, String> param = Maps.newHashMap();
 
         // 填入信息
         this.fillCommonInfo(param);
-        this.fillBalanceInfo(param, invoiceNo);
+        this.fillBalanceInfo(param, invoiceNo, placeCode);
 
         return result;
     }
@@ -58,9 +67,10 @@ public class BalanceService {
      * 填入结算信息
      * 
      * @param invoiceNo 发票号
-     * @param param 参数表
+     * @param param     参数表
+     * @param param     参数表
      */
-    private void fillBalanceInfo(Map<String, String> param, String invoiceNo) {
+    private void fillBalanceInfo(Map<String, String> param, String invoiceNo, String placeCode) {
         // 检索结算记录
         var wrapper = new QueryWrapper<FinIpbBalanceHead>().lambda();
         wrapper.eq(FinIpbBalanceHead::getInvoiceNo, invoiceNo);
@@ -70,7 +80,17 @@ public class BalanceService {
             throw new RuntimeException(String.format("未检索到结算信息，发票号：%s", invoiceNo));
         }
 
+        // 检索结算员
+        var balancer = this.dawnOrgEmplCache.get(balanceHead.getBalanceOperCode());
+        if (balancer == null) {
+            throw new RuntimeException(String.format("结算员不存在，工号：%s", balanceHead.getBalanceOperCode()));
+        }
+
         // 填写信息
         param.put("busNo", String.format("%s-%s", LocalDateTimeUtils.format(LocalDateTime.now(), "yyyyMMddHHmmssSSS"), invoiceNo));
+        param.put("busType", BusinessTypeEnum.住院.getDescription());
+        param.put("payer", balancer.getEmplName());
+        param.put("busDateTime", LocalDateTimeUtils.format(LocalDateTime.now(), "yyyyMMddHHmmssSSS"));
+        param.put("placeCode", placeCode);
     }
 }
