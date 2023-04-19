2f2c23905dcd6e5c7546129429a30d440825f699
nyakori <935419854@qq.com>
2023-04-19 15:10:51 +0800

    fix(tmp): 修改临时业务

--- src/main/java/com/kaos/walnut/api/logic/controller/TempController.java
+++ src/main/java/com/kaos/walnut/api/logic/controller/TempController.java
@@ -1,12 +1,15 @@
 package com.kaos.walnut.api.logic.controller;
 
-import java.util.Collection;
+import java.io.IOException;
 import java.util.Map;
 
 import javax.validation.Valid;
 
-import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,11 +17,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Maps;
-import com.google.common.collect.Multimap;
 import com.kaos.walnut.api.data.cache.DawnOrgDeptCache;
 import com.kaos.walnut.api.data.entity.DawnOrgEmpl;
+import com.kaos.walnut.api.data.enums.ValidStateEnum;
 import com.kaos.walnut.api.data.mapper.DawnOrgEmplMapper;
 import com.kaos.walnut.api.logic.service.surgery.privilege.DeptGrantService;
 import com.kaos.walnut.api.logic.service.surgery.privilege.EmplGrantService;
@@ -44,162 +46,79 @@ public class TempController {
     @Autowired
     DawnOrgEmplMapper dawnOrgEmplMapper;
 
-    /**
-     * 科室编码
-     */
-    private String deptId;
+    @ApiName("tmp")
+    @RequestMapping(value = "trans", method = RequestMethod.POST, produces = MediaType.EXCEL)
+    Object trans(@RequestBody @Valid Workbook workbook) throws IOException {
+        // 构造员工字典
+        Map<String, String> emplDict = Maps.newHashMap();
 
-    /**
-     * 职工字典
-     */
-    private Map<String, String> emplDict = Maps.newHashMap();
+        var result = new XSSFWorkbook();
+        Integer i = 0;
+        Integer j = 0;
+        try {
+            // 构造结果表单
+            var newSheet = result.createSheet("HIS号分析结果");
+            var header = newSheet.createRow(0);
+            header.createCell(0, CellType.STRING).setCellValue("主要编码");
+            header.createCell(1, CellType.STRING).setCellValue("附加编码");
+            header.createCell(2, CellType.STRING).setCellValue("手术名称");
+            header.createCell(3, CellType.STRING).setCellValue("类别");
+            header.createCell(4, CellType.STRING).setCellValue("录入选项");
+            header.createCell(5, CellType.STRING).setCellValue("手术级别");
 
-    @ApiName("切换科室")
-    @RequestMapping(value = "updateDept", method = RequestMethod.POST, produces = MediaType.JSON)
-    String updateDept(@RequestBody @Valid UpdateDept.ReqBody reqBody) {
-        var dept = this.dawnOrgDeptCache.get(reqBody.deptId);
-        if (dept == null) {
-            throw new RuntimeException("科室不存在-" + reqBody.deptId);
-        }
-        this.deptId = reqBody.deptId;
+            // 设置特殊背景色
+            var style = result.createCellStyle();
+            style.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+            style.setFillForegroundColor(IndexedColors.YELLOW.index);
 
-        return dept.getDeptName();
-    }
+            // 分析原始表单
+            var sheet = workbook.getSheetAt(0);
+            for (i = 1; i < sheet.getLastRowNum(); i++) {
+                var row = sheet.getRow(i);
+                var newRow = newSheet.createRow(i);
+                for (j = 0; j < row.getLastCellNum(); j++) {
+                    var cell = row.getCell(j);
+                    var newCell = newRow.createCell(j, CellType.STRING);
 
-    static class UpdateDept {
-        static class ReqBody {
-            String deptId;
-        }
-    }
+                    // 逻辑部分
+                    if (j < 5) {
+                        newCell.setCellValue(cell.toString());
+                    } else if (j == 5) {
+                        newCell.setCellValue(Double.valueOf(cell.toString()).intValue());
+                    } else {
+                        var emplName = cell.toString().trim().replaceAll(" ", "");
 
-    private String getEmplIdFromCell(Cell cell) {
-        // 判空
-        if (cell == null) {
-            return null;
-        }
+                        // 更新字典
+                        if (!emplDict.containsKey(emplName)) {
+                            var wrapper = new QueryWrapper<DawnOrgEmpl>().lambda();
+                            wrapper.eq(DawnOrgEmpl::getValidState, ValidStateEnum.在用);
+                            wrapper.eq(DawnOrgEmpl::getEmplName, emplName);
+                            var empls = this.dawnOrgEmplMapper.selectList(wrapper);
+                            if (empls.size() == 1) {
+                                emplDict.put(emplName, empls.get(0).getEmplCode());
+                            } else {
+                                emplDict.put(emplName, null);
+                            }
+                        }
 
-        String code = null;
-        switch (cell.getCellType()) {
-            case NUMERIC:
-                code = "" + Double.valueOf(cell.getNumericCellValue()).intValue();
-                break;
-
-            case STRING:
-                code = cell.getStringCellValue();
-                break;
-
-            default:
-                code = cell.toString();
-                break;
-        }
-        if (!code.startsWith("00")) {
-            code = "00" + code;
-        }
-        return code.trim();
-    }
-
-    @ApiName("更新职工字典")
-    @RequestMapping(value = "updateEmplDict", method = RequestMethod.POST, produces = MediaType.JSON)
-    Map<String, String> updateEmplDict(@RequestBody @Valid Workbook workbook) {
-        // 清空当前字典
-        this.emplDict.clear();
-
-        // 锚定表单
-        var sheet = workbook.getSheetAt(0);
-        for (Integer i = 4; i <= sheet.getLastRowNum(); i++) {
-            // 读取行
-            var row = sheet.getRow(i);
-            if (row == null) {
-                continue;
-            }
-
-            // 参数校验
-            var name = row.getCell(0) == null ? null : row.getCell(0).toString().trim();
-            var code = this.getEmplIdFromCell(row.getCell(2));
-            if (StringUtils.isBlank(name) || StringUtils.isBlank(code)) {
-                continue;
-            }
-
-            this.emplDict.put(name, code);
-        }
-
-        return this.emplDict;
-    }
-
-    @ApiName("医师授权")
-    @RequestMapping(value = "grant", method = RequestMethod.POST, produces = MediaType.JSON)
-    Map<String, Collection<String>> grant(@RequestBody @Valid Workbook workbook) {
-        // 创建辅助结构
-        Multimap<String, String> data = ArrayListMultimap.create();
-
-        // 锚定表单
-        var sheet = workbook.getSheetAt(0);
-        for (Integer i = 1; i <= sheet.getLastRowNum(); i++) {
-            // 读取行
-            var row = sheet.getRow(i);
-            if (row == null) {
-                continue;
-            }
-
-            // 读取ICD编码
-            String icdCode = null;
-            if (row.getCell(0) != null && !StringUtils.isBlank(row.getCell(0).toString())) {
-                icdCode = row.getCell(0).toString().trim();
-            } else if (row.getCell(1) != null && !StringUtils.isBlank(row.getCell(1).toString())) {
-                icdCode = row.getCell(1).toString().trim();
-            }
-            if (StringUtils.isBlank(icdCode)) {
-                continue;
-            }
-
-            // 读取医生
-            for (Integer j = 6; j <= row.getLastCellNum(); j++) {
-                // 读取单元格
-                var cell = row.getCell(j);
-
-                // 判空
-                if (cell == null || StringUtils.isBlank(cell.toString())) {
-                    continue;
+                        // 设置单元格
+                        var emplCode = emplDict.get(emplName);
+                        if (StringUtils.isBlank(emplCode)) {
+                            newCell.setCellValue(emplName);
+                            if (!StringUtils.isBlank(emplName)) {
+                                newCell.setCellStyle(style);
+                            }
+                        } else {
+                            newCell.setCellValue(emplCode);
+                        }
+                    }
                 }
-
-                // 记录结果
-                data.put(icdCode, cell.toString().trim());
             }
+        } catch (Exception ex) {
+            log.error(String.format("location: %d, %d", i, j));
+            throw new RuntimeException(ex.getMessage());
         }
 
-        // 授权过程
-        Integer count = 0;
-        for (var icdCode : data.asMap().keySet()) {
-            var docCodes = data.get(icdCode);
-
-            // 科室授权
-            this.deptGrantService.grant(icdCode, this.deptId);
-
-            // 医师授权
-            this.emplGrantService.grant(icdCode, docCodes.stream().map(x -> {
-                // 先取字典
-                if (this.emplDict.containsKey(x)) {
-                    return this.emplDict.get(x);
-                }
-
-                // 再查数据库
-                var wrapper = new QueryWrapper<DawnOrgEmpl>().lambda();
-                wrapper.eq(DawnOrgEmpl::getEmplName, x);
-                wrapper.eq(DawnOrgEmpl::getValidState, 1);
-                var empls = this.dawnOrgEmplMapper.selectList(wrapper);
-                if (empls.size() == 1) {
-                    this.emplDict.put(x, empls.get(0).getEmplCode());
-                    return this.emplDict.get(x);
-                }
-
-                throw new RuntimeException("医师字典不存在-" + x);
-            }).toList());
-
-            log.info(String.format("进度：%d/%d", ++count, data.asMap().keySet().size()));
-        }
-
-        log.info("授权完毕");
-
-        return data.asMap();
+        return result;
     }
 }
